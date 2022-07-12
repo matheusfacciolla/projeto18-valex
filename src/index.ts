d8eba024c82491910a5eb72461b61f2c27f2f01a
matheusfacciolla <matheustassifacciolla@gmail.com>
2022-07-12 01:26:20 -0300

    fix: fix index to deploy

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(json());
 app.use(router);
 app.use(handleErrorsMiddleware);
-const port: number = +process.env.PORT || 5000;
+const port = +process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(chalk.bold.green(`Server is running on port: ${port}`));
